b4aaceac94f43b9efd28a09eaaceafe10f39df9f
Mikhail <mkhrenov34@gmail.com>
2025-10-15 00:14:28 -0400

    Motor fields

--- ClosedHeaven/src/model/AxialFluxMotor.jl
+++ ClosedHeaven/src/model/AxialFluxMotor.jl
@@ -1,3 +1,8 @@
-struct AxialFluxMotor
-    
+struct AxialFluxMotor{T}
+    Np::Int     # Number of poles (multiple of 2)
+    Ns::Int     # Number of slots (multiple of 3)
+    Nt::Int     # Number of turns per slot
+    r_o::T      # Stator winding OD
+    r_i::T      # Stator winding ID
+    B::T        # Magnetic field strength, Tesla
 end
